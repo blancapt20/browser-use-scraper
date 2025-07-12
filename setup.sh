6e3f57956b03c9cfd6de5572356e981429c61d81
blancapuigt <blanca.puig@students.salle.url.edu>
2025-07-12 11:08:25 +0200

    Changed setup.sh after deleting dockerfile so i add the install-deps for the Chorium problem

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ set -e
 # Instalar los navegadores de Playwright (requerido en Railway y similares)
 echo "Instalando navegadores de Playwright..."
 playwright install
+playwright install-deps
 
 # (Opcional) Si usas Chromium específicamente:
 # playwright install chromium
